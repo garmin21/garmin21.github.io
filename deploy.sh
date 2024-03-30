54c5f20e25d77690ed15b8105fb1b2c3e4d7f538
garming <2180727350@qq.com>
2024-03-31 01:00:41 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 # 生成静态文件
 pnpm build
 
-cd dist
+# cd dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -20,14 +20,14 @@ git commit -m 'deploy'
 # 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f https://github.com/garmin21/jm-garming.git garming21:main
-git push https://github.com/garmin21/garmin21.github.io.git main -f
+# git push https://github.com/garmin21/garmin21.github.io.git main -f
 
-cd ..
+# git push https://github.com/garmin21/garmin21.github.io.git feature:develop
+
+git subtree push --prefix dist https://github.com/garmin21/garmin21.github.io.git main
+
+# cd ..
 
 rm -rf dist
-
-# git add -A
-# git commit -m '提交到主分支'
-# git push origin garming21 -f
 
 exit 0
