30dfd8743cecf1ac631eca6d58f154b83f3d35ae
garming <2180727350@qq.com>
2024-03-31 09:42:33 +0800

    21

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+rm -rf dist
+
 # 生成静态文件
 pnpm build
 
@@ -34,6 +36,8 @@ git commit -m 'deploy'
 
 
 # git pull
+
+git remote set-url origin git@github.com:garmin21/garmin21.github.io.git
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
