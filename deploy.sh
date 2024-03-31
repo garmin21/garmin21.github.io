2a7504ffb39fd9fc198c389029daea8391e0a034
lijiaming <garming21@163.com>
2024-03-31 21:23:42 +0800

    fix：shell 根据环境选择不同分支push

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,49 @@
 #!/usr/bin/env sh
 
+# 获取当前操作系统
+os=$(uname -s)
+
 # 确保脚本抛出遇到的错误
 set -e
 
 rm -rf dist
 
-# 生成静态文件
+echo '生成静态文件'
+
 pnpm build
 
 cd dist
 
-
+echo '====================================>'
 git init
 git add -A
 git commit -m 'deploy'
 
 
+echo '====================================>与远程分支建立联系'
 # 与远程分支建立联系
 git remote add origin git@github.com:garmin21/garmin21.github.io.git
 
+echo '====================================>强制推送到远程main 分支'
 # 强制推送到远程main 分支
-git push https://github.com/garmin21/garmin21.github.io.git garming21:main -f
+
+case $os in
+    # Linux*)
+    #     echo "Linux"
+    #     ;;
+    Darwin*)
+        # echo "macOS"
+        git push https://github.com/garmin21/garmin21.github.io.git garmin21:main -f
+        ;;
+    CYGWIN*|MINGW32*|MSYS*|MINGW*)
+        # echo "Windows"
+        git push https://github.com/garmin21/garmin21.github.io.git master:main -f
+        ;;
+    *)
+        echo "Unknown"
+        ;;
+esac
+# git push https://github.com/garmin21/garmin21.github.io.git master:main -f
 
 cd ..
 
